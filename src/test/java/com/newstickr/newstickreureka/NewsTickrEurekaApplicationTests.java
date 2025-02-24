03a1e0e1267e2ceab71d4b8d0a87ff2cc45e0423
DongHeon Kwak <rhkrehdgjs@naver.com>
2025-02-24 21:36:59 +0900

    init : 초기설정

---
+++ src/test/java/com/newstickr/newstickreureka/NewsTickrEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newstickr.newstickreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsTickrEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
